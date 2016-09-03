58c9f7f2cd54aa822154f2e7bba551bce7b28294
Willem Oosting <github@woosting.nl>
2016-09-03 14:01:26 +0200

    emptyCheck added + refactoring

--- dirp.sh
+++ dirp.sh
@@ -20,100 +20,120 @@
 # FORK ME AT GITHUB: https://github.com/woosting/dirp
 
 
+# TO-DO:
+
+#  function checkMount () {
+#    if mount | grep /mnt/md0 > /dev/null; then
+#      echo "yay"
+#    else
+#      echo "nay"
+#    fi
+#  }
+
+
 # CONFIGURATION:
-EMCOL='\033[1m'                 #EMPHASIS color (default: BOLD)
-NOKCOL='\033[0;31m'             #NOT OK color (default: RED)
-OKCOL='\033[0;32m'              #OK color (default: GREEN)
-RCOL='\033[0m'                  #RESET color (default: terminal default)
+
+  EMCOL='\033[1m'    #EMPHASIS color (default: BOLD)
+  NOKCOL='\033[0;31m' #NOT OK color (default: RED)
+  OKCOL='\033[0;32m' #OK color (default: GREEN)
+  RCOL='\033[0m'    #RESET color (default: terminal default)
+
 
 # INITIALISATION:
-ERRORS=()
 
-while getopts r:w: option     #CL-INTAKE (flagged arguments)
-do
-  case "${option}"
-   in
-    r) READABLE_DIRS+=(${OPTARG});;
-    w) WRITABLE_DIRS+=(${OPTARG});;
-  esac
-done
+  ERRORS=()
 
-if [ ${#READABLE_DIRS[@]} -gt 0 ]; then
-    DIRECTORIES[0]=${READABLE_DIRS[@]}
-fi
+  while getopts r:w: option     #CL-INTAKE (flagged arguments)
+  do
+    case "${option}"
+     in
+      r) DIRS2READ+=(${OPTARG});;
+      w) DIRS2WRITE+=(${OPTARG});;
+    esac
+  done
 
-if [ ${#WRITABLE_DIRS[@]} -gt 0 ]; then
-    DIRECTORIES[1]=${WRITABLE_DIRS[@]}
-fi
+  if [ ${#DIRS2READ[@]} -gt 0 ]; then
+    DIRS2CHECK[0]=${DIRS2READ[@]}
+  fi
 
-if [ ${#DIRECTORIES[@]} -eq 0 ]; then
+  if [ ${#DIRS2WRITE[@]} -gt 0 ]; then
+    DIRS2CHECK[1]=${DIRS2WRITE[@]}
+  fi
+
+  if [ ${#DIRS2CHECK[@]} -eq 0 ]; then
     echo -e "USAGE: dirp [-r /path] [-w /path]"
-    echo -e
-    echo -e "Input arguments are missing. Please check USAGE."
     exit 1;
-fi
+  fi
+
 
 # FUNCTION DEFINITION:
-function permissionOk() {
-    echo -e "${OKCOL}[\u2713]$RCOL ${1}"
-}
 
-function permissionError() {
-    ERRORS+=("${2} doesn't meet the requirement of ${1} (is ${PERM})")
-    echo -e "${NOKCOL}[!]$RCOL ${1}"
-}
+  function reportOK() {
+      echo -e "${OKCOL}[\u2713]$RCOL ${1}"
+  }
 
-function chkPerm() {
-    case ${1} in
-        read)
+  function reportNOK() {
+      ERRORS+=("${1} verified NOK: ${2}")
+      echo -e "${NOKCOL}[!]$RCOL ${1} ${2}"
+  }
+
+  function checkDir() {
+      if [ ! -d "$2" ]; then
+        reportNOK $2 "(not a directory)"
+      else
+        case ${1} in
+          read)
             if [ -r "${2}" ]; then
-                permissionOk ${2}
+              if [ ! "$(ls -A $2)"  ]; then
+                reportNOK $2 "(empty)"
+              else
+                reportOK ${2}
+              fi
             else
-                permissionError ${2}
+              reportNOK ${2}
             fi
-        ;;
-        write)
+          ;;
+          write)
             if [ -w "${2}" ]; then
-                    permissionOk ${2}
-                else
-                    permissionError ${2}
-                fi
-            ;;
-        *) permissionError ${2};;
-     esac
-}
+              if [ ! "$(ls -A $2)"  ]; then
+                reportNOK $2 "(empty)"
+              else
+                reportOK ${2}
+              fi
+            else
+              reportNOK ${2}
+            fi
+          ;;
+          *) reportNOK ${2};;
+        esac
+      fi
+  }
+
 
 # LOGIC EXECUTION:
-echo -e "Checking directory permissions..."
 
-for permission in ${!DIRECTORIES[@]}
-do
-    case ${permission} in
-        0) requirement=read;;
-        1) requirement=write;;
-    esac
+  for permission in ${!DIRS2CHECK[@]}
+  do
+      case ${permission} in
+          0) requirement="read";;
+          1) requirement="write";;
+      esac
 
-    echo -e "Checking ${EMCOL}${requirement^^}${RCOL} permissions for:"
+      echo -e "Checking ${EMCOL}${requirement^^}${RCOL} permissions:"
 
-    for directory in ${DIRECTORIES[${permission}]}
-    do
-        directory=$(readlink -f ${directory})
+      for directory in ${DIRS2CHECK[${permission}]}
+      do
+          directory=$(readlink -f ${directory})
+          checkDir ${requirement} ${directory}
+      done
+  done
 
-        if [ ! -d "${directory}" ]; then
-            echo -e "ERROR: The specified argument '${directory}' is not a directory."
-            exit 1
-        fi
+  if [ ${#ERRORS[@]} -eq 0 ]; then
+      echo -e "${EMCOL}PASSED${RCOL}: All directories are verified OK!"
 
-        chkPerm ${requirement} ${directory}
-    done
-done
+      exit 0
+  else
+      echo -e "${EMCOL}FAILED${RCOL}: some directories verified NOK!"
 
-if [ ${#ERRORS[@]} -eq 0 ]; then
-    echo -e "${EMCOL}PASSED${RCOL}"
-
-    exit 0
-else
-    echo -e "${EMCOL}FAILED${RCOL}"
-
-    exit 1
-fi
+      exit 1
+  fi
